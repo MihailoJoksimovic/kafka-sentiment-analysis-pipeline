5ac650c7fd3660a15a10bdf646f8c5505af89da9
Mihailo Joksimovic <mihailo.joksimovic@pool4tool.com>
2019-06-02 18:42:40 +0200

    Config file

---
+++ tweet-parser/config.py
@@ -0,0 +1,9 @@
+# Number of tweets to fetch when parse script is run
+TWEETER_NUM_TWEETS_TO_FETCH = 10
+
+# Name of the Kafka topic for storing raw tweets
+KAFKA_RAW_TWEETS_TOPIC = 'raw_tweets'
+
+# Name of the Kafka topic for storing processed tweets
+KAFKA_PROCESSED_TWEETS_TOPIC = 'processed_tweets'
+
